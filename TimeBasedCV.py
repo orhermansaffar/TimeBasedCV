4cbba1d09a1510502d0e356ecca74554761d69f5
orhermansaffar <59613363+orhermansaffar@users.noreply.github.com>
2020-01-19 17:32:04 +0200

    Update TimeBasedCV.py

--- TimeBasedCV.py
+++ TimeBasedCV.py
@@ -7,73 +7,173 @@ class TimeBasedCV(object):
     '''
     Parameters 
     ----------
-    train_period_days: int
-        number of days to include in each train set 
-    test_period_days: int
-        number of days to include in each test set 
+    train_period: int
+        number of time units to include in each train set
+        default is 30
+    test_period: int
+        number of time units to include in each test set
+        default is 7
+    freq: string
+        frequency of input parameters. possible values are: days, months, years, weeks, hours, minutes, seconds
+        possible values designed to be used by dateutil.relativedelta class
+        deafault is days
     '''
     
     
-    def __init__(self, train_period_days=30, test_period_days=7):
-        self.train_period_days = train_period_days
-        self.test_period_days = test_period_days 
+    def __init__(self, train_period=30, test_period=7, freq='days'):
+        self.train_period = train_period
+        self.test_period = test_period
+        self.freq = freq
 
         
         
-    def split(self, data, validation_split_date=None):
+    def split(self, data, validation_split_date=None, date_column='record_date', gap=0):
         '''
+        Generate indices to split data into training and test set
+        
         Parameters 
         ----------
-
-        Data: your data, contain one column for the record date named "record_date" 
-        validation_split_date: first date to perform the splitting on.
+        data: pandas DataFrame
+            your data, contain one column for the record date 
+        validation_split_date: datetime.date()
+            first date to perform the splitting on.
             if not provided will set to be the minimum date in the data after the first training set
-
+        date_column: string, deafult='record_date'
+            date of each record
+        gap: int, default=0
+            for cases the test set does not come right after the train set,
+            *gap* days are left between train and test sets
+        
         Returns 
         -------
         train_index ,test_index: 
-            index for train and test set similar to sklearn model selection
+            list of tuples (train index, test index) similar to sklearn model selection
         '''
+        
+        # check that date_column exist in the data:
+        try:
+            data[date_column]
+        except:
+            raise KeyError(date_column)
+                    
         train_indices_list = []
         test_indices_list = []
 
         if validation_split_date==None:
-            validation_split_date = data['record_date'].min().date() + relativedelta(days=self.train_period_days)
+            validation_split_date = data[date_column].min().date() + eval('relativedelta('+self.freq+'=self.train_period)')
+        
+        start_train = validation_split_date - eval('relativedelta('+self.freq+'=self.train_period)')
+        end_train = start_train + eval('relativedelta('+self.freq+'=self.train_period)')
+        start_test = end_train + eval('relativedelta('+self.freq+'=gap)')
+        end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
 
-        start_train = validation_split_date - relativedelta(days=self.train_period_days)
-        end_train = start_train + relativedelta(days=self.train_period_days)
-        start_test = end_train
-        end_test = start_test + relativedelta(days=self.test_period_days)
-
-        while end_test < data['record_date'].max().date():
+        while end_test < data[date_column].max().date():
             # train indices:
-            cur_train_indices = list(data[(data['record_date'].dt.date>=start_train) & 
-                                     (data['record_date'].dt.date<end_train)].index)
+            cur_train_indices = list(data[(data[date_column].dt.date>=start_train) & 
+                                     (data[date_column].dt.date<end_train)].index)
 
             # test indices:
-            cur_test_indices = list(data[(data['record_date'].dt.date>=start_test) &
-                                    (data['record_date'].dt.date<end_test)].index)
+            cur_test_indices = list(data[(data[date_column].dt.date>=start_test) &
+                                    (data[date_column].dt.date<end_test)].index)
+            
+            print("Train period:",start_train,"-" , end_train, ", Test period", start_test, "-", end_test,
+                  "# train records", len(cur_train_indices), ", # test records", len(cur_test_indices))
 
             train_indices_list.append(cur_train_indices)
             test_indices_list.append(cur_test_indices)
 
             # update dates:
-            start_train = start_train + relativedelta(days=self.test_period_days)
-            end_train = start_train + relativedelta(days=self.train_period_days)
-            start_test = end_train
-            end_test = start_test + relativedelta(days=self.test_period_days)
+            start_train = start_train + eval('relativedelta('+self.freq+'=self.test_period)')
+            end_train = start_train + eval('relativedelta('+self.freq+'=self.train_period)')
+            start_test = end_train + eval('relativedelta('+self.freq+'=gap)')
+            end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
 
         # mimic sklearn output  
         index_output = [(train,test) for train,test in zip(train_indices_list,test_indices_list)]
 
+        self.n_splits = len(index_output)
+        
         return index_output
+    
+    
+    def get_n_splits(self):
+        """Returns the number of splitting iterations in the cross-validator
+        Returns
+        -------
+        n_splits : int
+            Returns the number of splitting iterations in the cross-validator.
+        """
+        return self.n_splits 
 
 # How to use TimeBasedCV
 data_for_modeling=pd.read_csv('data.csv')
 data_for_modeling['record_date']=data_for_modeling['record_date'].values.astype('datetime64[D]')
-tscv = TimeBasedCV(train_period_days=10,
-                   test_period_days=3)
+tscv = TimeBasedCV(train_period=30,
+                   test_period=7,
+                   freq='days')
 for train_index, test_index in tscv.split(data_for_modeling,
-                   validation_split_date=datetime.date(2019,2,1)):
-    print(train_index)
-    print(test_index)
+                   validation_split_date=datetime.date(2019,2,1), date_column='record_date'):
+    continue
+
+# get number of splits
+tscv.get_n_splits()
+
+#### Example- compute average test sets score: ####
+X = data_for_modeling[['record_date',columns]]
+y = data_for_modeling[label]
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+scores = []
+for train_index, test_index in tscv.split(X, validation_split_date=datetime.date(2019,2,1)):
+
+    data_train   = X.loc[train_index].drop('record_date', axis=1)
+    target_train = y.loc[train_index]
+
+    data_test    = X.loc[test_index].drop('record_date', axis=1)
+    target_test  = y.loc[test_index]
+
+    # if needed, do preprocessing here
+
+    clf = LinearRegression()
+    clf.fit(data_train,target_train)
+
+    preds = clf.predict(data_test)
+
+    # accuracy for the current fold only    
+    r2score = clf.score(data_test,target_test)
+
+    scores.append(r2score)
+
+# this is the average accuracy over all folds
+average_r2score = np.mean(scores)
+#### End of example ####
+
+#### Example- RandomizedSearchCV ####
+from sklearn.model_selection import RandomizedSearchCV
+from lightgbm import LGBMRegressor
+from random import randint, uniform
+
+tscv = TimeBasedCV(train_period=10, test_period=3)
+index_output = tscv.split(data_for_modeling, validation_split_date=datetime.date(2019,2,1))
+
+lgbm = LGBMRegressor()
+
+lgbmPd = {" max_depth": [-1,2]
+         }
+
+model = RandomizedSearchCV(
+    estimator = lgbm,
+    param_distributions = lgbmPd,
+    n_iter = 10,
+    n_jobs = -1,
+    iid = True,
+    cv = index_output,
+    verbose=5,
+    pre_dispatch='2*n_jobs',
+    random_state = None,
+    return_train_score = True)
+
+model.fit(X.drop('record_date', axis=1),y)
+model.cv_results_
+#### End of example ####
